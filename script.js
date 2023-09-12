3a950540bdcdffbd71b48d4063d84513bb72fe37
Sourabh Pathak <63462210+sourabh1428@users.noreply.github.com>
2023-09-12 23:58:17 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,5 +1,22 @@
 function firstNonRepeatedChar(str) {
  // Write your code here
+	for(let i=0;i<str.length;i++){
+    if(map.has(str.charAt(i))){
+        map.set(str.charAt(i),map.get(str.charAt(i))+1);
+    }
+    else{
+        map.set(str.charAt(i),1);
+    }
 }
-const input = prompt("Enter a string");
-alert(firstNonRepeatedChar(input)); 
+
+for(let i=0;i<str.length;i++){
+    if(map.get(str.charAt(i))==1){
+        return str.charAt(i);
+    }
+}
+return null;
+
+	
+}
+// const input = prompt("Enter a string");
+// alert(firstNonRepeatedChar(input)); 
